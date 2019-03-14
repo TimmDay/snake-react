e4a430e282ca26aefb5d419c2369467f23d81516
TimmDay <timmday.info@gmail.com>
2019-03-14 23:16:25 +0100

    impl function to generate random stars in the header background

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-export const Header = (props) => (
-  <header>
-    <h1>Fizz Buzz Snake</h1>
-    <p>Mathematical turmoil has engulfed the Galapplexy</p>
-  </header>
-);
+export const Header = () => {
+  const numStars = 80;
+  const getRandomPosition = () => {
+    var x = window.innerWidth; //header spans the screen
+    var randomX = Math.floor(Math.random() * x);
+    var randomY = Math.floor(Math.random() * 180); //needs to match header height in css
+    return [randomX, randomY];
+  };
+  for (let i = 0; i < numStars; i++) {
+    let star = document.createElement('div');
+    star.className = 'star';
+    const xy = getRandomPosition();
+    star.style.left = xy[0] + 'px';
+    star.style.top = xy[1] + 'px';
+    document.body.append(star);
+  }
+
+  return (
+    <header>
+      <h1>Fizz Buzz Snake</h1>
+      <p>Mathematical turmoil has engulfed the Galapplexy</p>
+    </header>
+  );
+};
 
 export default Header;
