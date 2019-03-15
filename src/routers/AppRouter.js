933b8b7c9168c85fb7cb43096ba0892b3db33ece
TimmDay <timmday.info@gmail.com>
2019-03-15 09:35:49 +0100

    heroku troubleshoot

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import Header from '../components/Header';
-import Snake from './../components/Snake';
+import Snake from './../components/Snake.js';
 import SideBar from './../components/SideBar';
 import SideBarRight from './../components/SideBarRight';
 
-// export const history = createHistory();
+export const history = createHistory();
 
 const AppRouter = () => (
-  <Router>
+  <Router history={history}>
     <div>
       <Header />
       <main>
       < SideBar />
       <Switch>
-        <Route path="/" component={Snake} />
+        <Route path="/" component={Snake} exact={true} />
       </Switch>
       < SideBarRight />
       </main>
