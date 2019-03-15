f06a1d50e3bdebfdf60baa32012fb6aa6ae54a37
TimmDay <timmday.info@gmail.com>
2019-03-15 12:43:26 +0100

    ts config

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,9 @@ module.exports = (env) => {
         })
       }]
     },
+    plugins: [
+      CSSExtract
+    ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
