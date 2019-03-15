a3b0038efd86d63baa51a0376b5ec6488f4beee2
TimmDay <timmday.info@gmail.com>
2019-03-15 09:26:33 +0100

    heroku paths change

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import Header from '../components/Header';
-import Snake from '../components/Snake';
-import SideBar from '../components/SideBar';
-import SideBarRight from '../components/SideBarRight';
+import Snake from './../components/Snake';
+import SideBar from './../components/SideBar';
+import SideBarRight from './../components/SideBarRight';
 
 export const history = createHistory();
 
