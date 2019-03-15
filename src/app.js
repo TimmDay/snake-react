28318f430026cb3272008ba432ffbca79e82ad22
TimmDay <timmday.info@gmail.com>
2019-03-15 12:55:17 +0100

    ts config

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
 const jsx = (
@@ -19,4 +19,4 @@ ReactDOM.render(jsx, document.getElementById('app'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
